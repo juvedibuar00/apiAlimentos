9865961683225b9d98ecd6442527308c7ad2939a
nathana372@gmail.com <nathana372@gmail.com>
2024-06-15 17:36:13 -0300

    atualizando

---
+++ alimentos.py
@@ -0,0 +1,19 @@
+#REPRESENTAÇAO DA CLASSE ALIMENTOS
+from pydantic import BaseModel
+class Alimento:
+    def __init__(self,  id: int,  nome: str, preco: float, disponibilidade: bool,quantidade: int, imagem: str ):
+        self.id = id
+        self.nome = nome
+        self.preco = preco
+        self.disponibilidade = disponibilidade
+        self.quantidade = quantidade
+        self.imagem = imagem
+
+
+alimento1 = Alimento(1, 'Banana', 10.50, True, 10, '')
+
+print(alimento1.nome)
+
+
+
+
